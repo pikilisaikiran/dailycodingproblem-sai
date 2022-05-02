16e72387c029ed7629a1bc09e815ebaae323bc6a
pikilisaikiran <96593012+pikilisaikiran@users.noreply.github.com>
2022-05-02 10:39:00 +0530

    Add files via upload

---
+++ problem-1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int find(int a[], int n, int k)
+{   int f=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]+a[j]==k)
+            {
+                f=1;
+                break;
+            }
+
+        }
+    }
+
+    return f;
+}
+int main()
+{
+    int n,k;
+    printf("Enter the number of element : ");
+    scanf("%d",&n);
+    printf("Enter the value of k : ");
+    scanf("%d",&k);
+    int a[n];
+    printf("Enter the element : ");
+    for(int i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    if(find(a,n,k)==1)
+    printf("true");
+    else
+    printf("false");
+    return 0;
+}
