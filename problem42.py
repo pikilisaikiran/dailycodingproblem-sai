a5c757bcd7cc5784dcab03134382d1b0ab27130a
pikilisaikiran <96593012+pikilisaikiran@users.noreply.github.com>
2022-06-22 09:11:40 +0530

    Add files via upload

---
+++ problem42.py
@@ -0,0 +1,23 @@
+"""
+Given a list of integers S and a target number k, write a function that returns 
+a subset of S that adds up to k. If such a subset cannot be made, then return null.
+
+Integers can appear more than once in the list. You may assume all numbers in the list are positive.
+
+For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
+"""
+def find_subset(s,k):
+    if not s: 
+        return 
+    num=s[0] 
+    if num==k:
+        return [num]
+    subset=find_subset(s[1:],k-num) 
+    if subset is not None:
+        return [num] + subset 
+    return find_subset(s[1:],k)
+#driver code
+s = [12, 1, 61, 5, 9, 2]
+k = 25
+memo={}
+print(find_subset(s,k))
